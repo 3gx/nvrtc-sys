ddde0716b9cd24c370a01244f149f856161ff0cb
Tom Fogal <tfogal@sci.utah.edu>
2018-10-16 22:25:55 -0700

    initial commit, basics partially working.

---
+++ build.rs
@@ -0,0 +1,51 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+	let cuda_path = PathBuf::from(match env::var("CUDA_HOME") {
+		Ok(chome) => chome,
+		Err(_) => "/usr/local/cuda".to_string()
+	});
+	let cuda = match cuda_path.to_str() {
+		Some(c) => c,
+		None => "nvrtc-sys: error creating string from cuda path",
+	};
+	for libdir in vec!["lib64", "lib"] {
+		let mut clib_path = cuda_path.clone();
+		clib_path.push(libdir);
+		// Don't check if the path exists first.  If someone is having issues and
+		// turns verbosity on, this will at least clue them in how to hack it.
+		println!("cargo:rustc-link-search=native={}/{}", cuda, libdir);
+	}
+	println!("cargo:rustc-link-lib=nvrtc"); // link against nvrtc.
+
+	// The bindgen::Builder is the main entry point
+	// to bindgen, and lets you build up options for
+	// the resulting bindings.
+	let bindings = bindgen::Builder::default()
+			// Tell clang where to find cuda.h.
+			.clang_arg(format!("-I{}/include", cuda))
+			.header("nvrtc-sys.h")
+			.whitelist_recursively(true) // FIXME set to false
+			.whitelisted_type("nvrtcResult")
+			.whitelisted_type("nvrtcProgram")
+			.whitelisted_function("nvrtcCompileProgram")
+			.whitelisted_function("nvrtcCreateProgram")
+			.whitelisted_function("nvrtcDestroyProgram")
+			.whitelisted_function("nvrtcGetErrorString")
+			.whitelisted_function("nvrtcGetProgramLogSize")
+			.whitelisted_function("nvrtcGetProgramLog")
+			.whitelisted_function("nvrtcGetPTX")
+			.whitelisted_function("nvrtcGetPTXSize")
+			.whitelisted_function("nvrtcVersion")
+			.generate()
+			.expect("Unable to generate nvrtc-sys bindings");
+
+	// Write the bindings to the $OUT_DIR/bindings.rs file.
+	let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+	bindings
+			.write_to_file(out_path.join("nvrtc.rs"))
+			.expect("Couldn't write nvrtc-sys bindings!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+pub mod nvrtc;
+
+#[cfg(test)]
+mod tests {
+	use std::os::raw;
+	use std::ffi::CString;
+	use nvrtc::*;
+
+	fn vecadd_src() -> CString {
+		return CString::new("extern \"C\" __global__ void\n".to_string() +
+			"addfv(const float* a, const float* b, float* out, size_t n) {\n" +
+			"	size_t i = blockDim.x*blockIdx.x + threadIdx.x;\n" +
+			"	if(i >= n) {\n" +
+			"		return;\n" +
+			"	}\n" +
+			"	out[i] = a[i] + b[i]\n" +
+			"}\n"
+		).unwrap();
+	}
+
+	// A macro that expects an nvrtc call to succeed.
+	macro_rules! rtc {
+		($call:expr) => ({
+			let res: nvrtcResult = $call;
+			assert_eq!(res, nvrtcResult::NVRTC_SUCCESS);
+		})
+	}
+
+	macro_rules! compile {
+		($prg:expr, $call:expr) => ({
+			let res: nvrtcResult = $call;
+			if res != nvrtcResult::NVRTC_SUCCESS {
+				let mut size : usize = 0;
+				let sizeptr: *mut usize = &mut size as *mut usize;
+				let retsz: nvrtcResult = nvrtcGetProgramLogSize($prg, sizeptr);
+				if retsz != nvrtcResult::NVRTC_SUCCESS {
+					eprintln!("after error {:?}, error {:?} getting program log size",
+					          res, retsz);
+					return;
+				}
+				println!("program log is {} bytes.", size);
+			}
+		})
+	}
+
+	#[test]
+	fn create_destroy() {
+		let name = CString::new("vecadd").unwrap();
+		let source = vecadd_src();
+		let num_hdr: ::std::os::raw::c_int = 0;
+		let mut headers: Vec<*const raw::c_char> = vec![];
+		let mut incl: Vec<*const raw::c_char> = vec![];
+
+		unsafe {
+		let mut prg: nvrtcProgram = ::std::mem::uninitialized();
+		// headers and incl need to be mutably borrowed, even though they would
+		// never modify their arguments.  It's not bindgen's fault; nvrtc's header
+		// declares them in a way that they look mutable.
+		rtc!(nvrtcCreateProgram(&mut prg, source.as_ptr(), name.as_ptr(), num_hdr,
+		                        headers.as_mut_ptr(), incl.as_mut_ptr()));
+		rtc!(nvrtcDestroyProgram(&mut prg));
+		}
+	}
+
+	// Compilation without any headers.
+	#[test]
+	fn compile_no_hdr() {
+		let name = CString::new("vecadd").unwrap();
+		let source = vecadd_src();
+		let num_hdr: raw::c_int = 0;
+		let mut headers: Vec<*const raw::c_char> = vec![];
+		let mut incl: Vec<*const raw::c_char> = vec![];
+		let mut options: Vec<*const raw::c_char> = vec![];
+
+		unsafe {
+		let mut prg: nvrtcProgram = ::std::mem::uninitialized();
+		rtc!(nvrtcCreateProgram(&mut prg, source.as_ptr(), name.as_ptr(), num_hdr,
+		                        headers.as_mut_ptr(), incl.as_mut_ptr()));
+		compile!(prg, nvrtcCompileProgram(prg, 0 as raw::c_int,
+		                                  options.as_mut_ptr()));
+		rtc!(nvrtcDestroyProgram(&mut prg));
+		}
+	}
+}
